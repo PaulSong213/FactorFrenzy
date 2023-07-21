0ad5c717719eeb49c786a3b69277e91b77529232
John Paul Songalia <paul.songalia@edufied.edu.vn>
2023-07-22 00:11:46 +0800

    Add background music

--- App.js
+++ App.js
@@ -5,8 +5,24 @@ import HomeScreen from "./Screens/Home/HomeScreen";
 import GameScreen from "./Screens/Game/GameScreen";
 import LessonScreen from "./Screens/Lesson/LessonScreen";
 const Stack = createNativeStackNavigator();
+import { Audio } from "expo-av";
 
 export default function App() {
+  const [sound, setSound] = React.useState();
+
+  async function playSound() {
+    console.log("Loading Sound");
+    const { sound } = await Audio.Sound.createAsync(require("./bg-music.mp3"));
+    sound.setIsLoopingAsync(true);
+    setSound(sound);
+
+    console.log("Playing Sound");
+    await sound.playAsync();
+  }
+
+  React.useEffect(() => {
+    playSound();
+  }, []);
   return (
     <NavigationContainer>
       <Stack.Navigator
